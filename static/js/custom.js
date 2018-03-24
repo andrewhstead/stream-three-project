c24c1fe7b7f7430d9c5d34379cc9449f78458887
Andrew Stead <andrew@andrewstead.co.uk>
2018-03-24 20:12:44 +0000

    Added menu toggle for main site navigation

---
+++ static/js/custom.js
@@ -0,0 +1,4 @@
+// Function to allow the user to toggle the navigation menu on and off in the default stylesheet.
+function menuToggle() {
+	document.getElementById("main-menu").classList.toggle("activate");
+}
