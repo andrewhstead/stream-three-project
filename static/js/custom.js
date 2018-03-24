af7f7398008636adb3cd8dd38b2aaab46edd76d9
Andrew Stead <andrew@andrewstead.co.uk>
2018-03-24 23:38:23 +0000

    Added jQuery slide toggle on main menu

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,4 @@
 // Function to allow the user to toggle the navigation menu on and off in the default stylesheet.
-function menuToggle() {
-	document.getElementById("main-menu").classList.toggle("activate");
-}
+$('#menu-button').click(function(){
+    $('#main-menu').slideToggle(1000);
+});
