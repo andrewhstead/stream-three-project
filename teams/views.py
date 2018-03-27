a19e9b3723c16d63f4e3eb0e7014a6da7403d7c6
Andrew Stead <andrew@andrewstead.co.uk>
2018-03-27 14:16:07 +0100

    Added last and next game boxes to team profile pages

--- teams/views.py
+++ teams/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from .models import Team, Conference
 from news.models import Item
+from games.models import Game
 
 
 # Create your views here.
@@ -15,4 +16,20 @@ def team_index(request):
 def team_page(request, team_name):
     items = Item.objects.all()
     team = get_object_or_404(Team, geographic_name=team_name.capitalize())
-    return render(request, "profile.html", {'team': team, 'items': items})
+
+    games = Game.objects.all().order_by('game_date')
+    team_results = []
+    team_fixtures = []
+
+    for game in games:
+        if game.home_team == team or game.away_team == team:
+            if game.game_status == "Completed":
+                team_results.append(game)
+            if game.game_status == "Scheduled":
+                team_fixtures.append(game)
+
+    last_game = team_results[-1]
+    next_game = team_fixtures[0]
+
+    return render(request, "profile.html", {'team': team, 'items': items,
+                                            'next_game': next_game, 'last_game': last_game})
