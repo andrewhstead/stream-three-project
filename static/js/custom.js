f5da40f5735b052e9048e690b146b999e10539b9
Andrew Stead <andrew@andrewstead.co.uk>
2018-03-30 20:59:15 +0100

    Added function to toggle display of comments

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,3 +2,8 @@
 $('#menu-button').click(function(){
     $('#main-menu').slideToggle(1000);
 });
+
+// Function to allow the user to toggle the comments on and off.
+$('#show-hide').click(function(){
+    $('#comments').fadeToggle(500);
+});
