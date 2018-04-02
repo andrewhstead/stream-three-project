0b16dc826b79692721d550d200bfeec6f1838d0a
Andrew Stead <andrew@andrewstead.co.uk>
2018-04-02 14:10:59 +0100

    Added jQuery confirmation for deleting a post or comment

--- static/js/custom.js
+++ static/js/custom.js
@@ -7,3 +7,9 @@ $('#menu-button').click(function(){
 $('#show-hide').click(function(){
     $('#comments').fadeToggle(500);
 });
+
+// Function to allow the user to confirm that they wish to delete a post or a comment.
+$('.delete-link').click(function(){
+    $('.lights-down').fadeToggle(100);
+    $('.delete-confirm').fadeToggle(100);
+});
