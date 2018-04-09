84208bd2ec46a14f82effadf24d0a9109c0ddde6
Andrew Stead <andrew@andrewstead.co.uk>
2018-04-10 00:02:19 +0100

    Added news headline scroller on Home Page

--- home/views.py
+++ home/views.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from teams.models import Team
+from news.models import Item
 
 
 # Create your views here.
 def home_page(request):
-    return render(request, "home.html")
+
+    news = Item.objects.exclude(category_id=6).order_by('-created_date')
+
+    return render(request, "home.html", {'news': news})
---
+++ static/js/news_fader.js
@@ -0,0 +1,36 @@
+/* Variable set to 0 to ensure the slideshow starts with the first photo in the array. Initial value set outside the function so as not to interfere with incremented value when the function is running. */
+var leadStory = 0;
+
+/* Function to run the image slider. */
+function newsFader() {
+
+	/* Create an array containing all the slider image elements. */
+	var newsFader = document.getElementsByClassName("banner-story");
+	var newsBlobs = document.getElementsByClassName("news-blob");
+
+	/* Hide each element by giving it the class 'hiddenContent'and ensuring no elements have the class 'blockContent'. */
+	for (var i = 0; i < newsFader.length; i++) {
+		newsFader[i].classList.add("hidden");
+		newsFader[i].classList.remove("active");
+		newsBlobs[i].classList.add("default-blob");
+		newsBlobs[i].classList.remove("active-blob");
+	}
+
+	/* If the currentImage variable has passed the end of the array, return it to the beginning. */
+	if (leadStory >= newsFader.length) {
+		leadStory = 0;
+	}
+
+	/* Display the current image by removing the class 'hiddenContent'. Add the class 'blockContent' to ensure that the image displays as a block element. */
+	newsFader[leadStory].classList.remove("hidden");
+	newsFader[leadStory].classList.add("active");
+	newsBlobs[leadStory].classList.remove("default-blob");
+	newsBlobs[leadStory].classList.add("active-blob");
+
+	/* Increment the currentImage variable in preparation for the next transition. */
+	leadStory++;
+}
+
+/* Run the function on page load and then again at 10 second intervals. */
+newsFader();
+setInterval(newsFader,10000);
