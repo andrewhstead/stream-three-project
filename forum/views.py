a7f13137f45b0e037871d80412cd65bad9c0034d
Andrew Stead <andrew@andrewstead.co.uk>
2018-04-18 15:40:44 +0100

    Styled forum pages for desktop view

--- forum/views.py
+++ forum/views.py
@@ -19,13 +19,16 @@ from django.utils import timezone
 def forum_home(request):
     sections = Section.objects.all()
     boards = Board.objects.all()
-    return render(request, 'forum.html', {'sections': sections, 'boards': boards})
+    recent_posts = Post.objects.all().order_by('-created_date')[:10]
+
+    return render(request, 'forum.html', {'sections': sections, 'boards': boards, 'recent_posts': recent_posts})
 
 
 def forum_team(request, team_name):
     team = get_object_or_404(Team, geographic_name=team_name.capitalize())
     board = get_object_or_404(Board, team=team)
     all_threads = board.threads.all().order_by('-last_post')
+    recent_posts = Post.objects.all().order_by('-created_date')[:10]
 
     page_threads = Paginator(all_threads, 20)
 
@@ -38,12 +41,13 @@ def forum_team(request, team_name):
     except PageNotAnInteger:
         threads = page_threads.page(1)
 
-    return render(request, 'board.html', {'board': board, 'team': team, 'threads': threads})
+    return render(request, 'board.html', {'board': board, 'team': team, 'threads': threads, 'recent_posts': recent_posts})
 
 
 def forum_league(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
     all_threads = board.threads.all().order_by('-last_post')
+    recent_posts = Post.objects.all().order_by('-created_date')[:10]
 
     page_threads = Paginator(all_threads, 20)
 
@@ -56,12 +60,13 @@ def forum_league(request, board_id):
     except PageNotAnInteger:
         threads = page_threads.page(1)
 
-    return render(request, 'board.html', {'board': board, 'threads': threads})
+    return render(request, 'board.html', {'board': board, 'threads': threads, 'recent_posts': recent_posts})
 
 
 @login_required(login_url='/login/')
 def new_thread(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
+    recent_posts = Post.objects.all().order_by('-created_date')[:10]
 
     if request.method == 'POST':
         thread_form = ThreadForm(request.POST)
@@ -94,7 +99,8 @@ def new_thread(request, board_id):
         'thread_form': thread_form,
         'post_form': post_form,
         'board': board,
-        'button_text': 'Start Thread'
+        'button_text': 'Start Thread',
+        'recent_posts': recent_posts
     }
     args.update(csrf(request))
 
@@ -104,6 +110,7 @@ def new_thread(request, board_id):
 def view_thread(request, thread_id):
     thread = get_object_or_404(Thread, pk=thread_id)
     board = get_object_or_404(Board, pk=thread.board_id)
+    recent_posts = Post.objects.all().order_by('-created_date')[:10]
 
     # URL used to ensure thread.views is only incremented once if a user browses a paginated thread.
     url = request.get_full_path()
@@ -135,16 +142,18 @@ def view_thread(request, thread_id):
     if board.team_id:
         team = get_object_or_404(Team, pk=board.team_id)
         return render(request, 'thread.html', {'thread': thread, 'board': board, 'team': team,
-                                               'posts': posts, 'page': page, 'previous': previous})
+                                               'posts': posts, 'page': page, 'previous': previous,
+                                               'recent_posts': recent_posts})
 
     else:
         return render(request, 'thread.html', {'thread': thread, 'board': board, 'posts': posts,
-                                               'page': page, 'previous': previous})
+                                               'page': page, 'previous': previous, 'recent_posts': recent_posts})
 
 
 @login_required(login_url='/login/')
 def new_post(request, thread_id):
     thread = get_object_or_404(Thread, pk=thread_id)
+    recent_posts = Post.objects.all().order_by('-created_date')[:10]
 
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -167,6 +176,7 @@ def new_post(request, thread_id):
     args = {
         'form': form,
         'thread': thread,
+        'recent_posts': recent_posts,
         'button_text': 'Submit Post'
     }
     args.update(csrf(request))
@@ -178,6 +188,7 @@ def new_post(request, thread_id):
 def edit_post(request, thread_id, post_id):
     thread = get_object_or_404(Thread, pk=thread_id)
     post = get_object_or_404(Post, pk=post_id)
+    recent_posts = Post.objects.all().order_by('-created_date')[:10]
 
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
@@ -196,6 +207,7 @@ def edit_post(request, thread_id, post_id):
         'thread': thread,
         'form_action': reverse('edit_post', kwargs={'thread_id': thread.id, 'post_id': post.id}),
         'button_text': 'Edit Post',
+        'recent_posts': recent_posts,
         'post': post
     }
     args.update(csrf(request))
