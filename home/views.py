851c04b04361262d00684501ee680acfc1023648
Andrew Stead <andrew@andrewstead.co.uk>
2018-04-23 00:13:06 +0100

    Styled statistics bar to highlight user's favourite team

--- home/views.py
+++ home/views.py
@@ -11,6 +11,10 @@ from datetime import datetime
 # Create your views here.
 def home_page(request):
 
+    user = request.user
+    favourite_team = user.favourite_team
     news_headlines = Item.objects.exclude(category_id=6).order_by('-created_date')[:7]
+    team_news = Item.objects.filter(teams=favourite_team.id).order_by('-created_date')[:5]
 
-    return render(request, "home.html", {"news_headlines": news_headlines})
+    return render(request, "home.html", {"news_headlines": news_headlines, "favourite_team": favourite_team,
+                                         "team_news": team_news})
